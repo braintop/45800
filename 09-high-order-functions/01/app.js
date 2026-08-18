fe5fb56534c5178f735ddb059e1538c28a63c768
braintop <braintop7@gmail.com>
2026-08-18 12:43:48 +0300

    Add

---
+++ 09-high-order-functions/01/app.js
@@ -0,0 +1,39 @@
+let numbers = [1, 2, 3, 4, 5,59, 80, 1];
+
+// let squers = [] 
+// for (let i = 0; i < numbers.length; i++) {
+//     squers.push(numbers[i] * numbers[i]);
+// }
+// console.log(squers);//[1, 4, 9, 16, 25]
+
+let squers1 = numbers.map(number=> number * number );
+let squers2 = numbers.map(number=>{
+    return number * number;
+});
+
+console.log(squers1);//[1, 4, 9, 16, 25]
+console.log(squers2);//[1, 4, 9, 16, 25]
+
+
+let numbers1 = numbers.find(number=> number > 3);
+console.log(numbers1);//4
+
+let numbers2 = numbers.findIndex(number=> number > 3);
+console.log(numbers2);//1
+
+let numbers3 = numbers.filter(number=> number > 3);
+console.log(numbers3);//[4, 5, 59, 80]
+
+
+numbers.forEach(number=>{
+    console.log(number*number + number);//1, 6, 10, 17, 26, 26, 6400, 2
+});
+
+
+
+let numbers4 = numbers.map(number=> number*number).filter(number=> number > 3).forEach(number=>{
+    console.log(number);
+})  ;
+
+
+
